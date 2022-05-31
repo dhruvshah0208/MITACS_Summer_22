30e775bc4c569b3c3d62125a6dfba69067d2ff64
Dhruv Shah <55907313+dhruvshah0208@users.noreply.github.com>
2022-05-30 18:28:37 -0700

    Backward error analysis begun

--- Codes/cartPendulum.m
+++ Codes/cartPendulum.m
@@ -11,20 +11,20 @@
 (**)
 (*(*Exported symbols added here with SymbolName::usage*)*)
 (**)
-(*ffCartPendulumGeneral::usage = "*)
-(*ffCartPendulumGeneral[n_,\[Tau]_,\[Tau]1_,A_]*)
+(*ffCartPendulum::usage = "*)
+(*ffCartPendulum[ICs_,n_,\[Tau]_,\[Tau]1_,A_]*)
 (*Computes the feedforward state and costate trajectories for swinging up the pendulum optimally*)
 (*{xff,xdotff,\[Theta]ff,\[Theta]dotff,uff}*)
 (*";*)
 (**)
-(*TestSwingUpGeneral::usage = "*)
-(*TestSwingUpGeneral[\[Tau]_,\[Tau]1_,uff0_,A_]*)
+(*testSwingUp::usage = "*)
+(*testSwingUp[ICs_,\[Tau]_,\[Tau]1_,uff0_,A_]*)
 (*Test the approximate solution on the open-loop dynamics*)
 (*{xs,\[Theta]s,xdots,\[Theta]dots}*)
 (*";*)
 (**)
 (*TestSwingUpGeneralFB::usage = "*)
-(*TestSwingUpGeneralFB[\[Tau]_,\[Tau]1_,xff0_,xdotff0_,\[Theta]ff0_,\[Theta]dotff0_,uff0_,A_]*)
+(*TestSwingUpGeneralFB[ICs_,\[Tau]_,\[Tau]1_,xff0_,xdotff0_,\[Theta]ff0_,\[Theta]dotff0_,uff0_,A_]*)
 (*Add Linear feedback only at the end *)
 (*{xs,\[Theta]s,us}*)
 (*";*)
@@ -36,19 +36,19 @@
 (*";*)
 (**)
 (*TestSwingUpGeneralFBNumeric::usage = "*)
-(*TestSwingUpGeneralFBNumeric[\[Tau]_,\[Tau]1_,xff0_,xdotff0_,\[Theta]ff0_,\[Theta]dotff0_,uff0_,A_,KTable_,n_]*)
+(*TestSwingUpGeneralFBNumeric[ICs_,\[Tau]_,\[Tau]1_,xff0_,xdotff0_,\[Theta]ff0_,\[Theta]dotff0_,uff0_,A_,KTable_,n_]*)
 (*Simulates the true dynamics with feedback added according to Ktable*)
 (*{xs,\[Theta]s,us,K1,K2,K3,K4}*)
 (*";*)
 (**)
-(*SimulateLinearFeedbackEnd::usage = "*)
-(*SimulateLinearFeedbackEnd[ICs_,n_,\[Tau]_,\[Tau]1_,A_]*)
+(*simulateLinearFeedbackEnd::usage = "*)
+(*simulateLinearFeedbackEnd[ICs_,n_,\[Tau]_,\[Tau]1_,A_]*)
 (*Outputs plots directly using TestSwingUpGeneralFB*)
 (*Grid[{{p1a,p1b,p1c}}]*)
 (*";*)
 (**)
-(*SimulateLQRFeedback::usage = "*)
-(*SimulateLQRFeedback[ICs_,n_,n2_,\[Tau]_,\[Tau]1_,A_]*)
+(*simulateLQRFeedback::usage = "*)
+(*simulateLQRFeedback[ICs_,n_,n2_,\[Tau]_,\[Tau]1_,A_]*)
 (*Outputs plots directly using TestSwingUpGeneralFBNumeric*)
 (*Grid[{{p1a,p1b,p1c}}]*)
 (*";*)
@@ -61,7 +61,7 @@
 (*Begin["`Private`"]*)
 (**)
 (*(*ICs - Initial Conditions *)*)
-(*ffCartPendulumGeneral[ICs_,n_,\[Tau]_,\[Tau]1_,A_]:=Module[{x,xdot,f,\[Theta],\[Theta]dot,\[Lambda]1,\[Lambda]2,\[Lambda]3,\[Lambda]4,\[CapitalDelta]t,bcs,eqns,sv,froot,xff,xdotff,xff0,xdotff0,\[Theta]ff0,\[Theta]dotff0,uff0,\[Theta]ff,\[Theta]dotff,uff},\[CapitalDelta]t=\[Tau]/n;*)
+(*ffCartPendulum[ICs_,n_,\[Tau]_,\[Tau]1_,A_]:=Module[{x,xdot,f,\[Theta],\[Theta]dot,\[Lambda]1,\[Lambda]2,\[Lambda]3,\[Lambda]4,\[CapitalDelta]t,bcs,eqns,sv,froot,xff,xdotff,xff0,xdotff0,\[Theta]ff0,\[Theta]dotff0,uff0,\[Theta]ff,\[Theta]dotff,uff},\[CapitalDelta]t=\[Tau]/n;*)
 (*f[{x_,xdot_,\[Theta]_,\[Theta]dot_,\[Lambda]1_,\[Lambda]2_,\[Lambda]3_,\[Lambda]4_}]:={xdot,1/(1-A Cos[\[Theta]]^2) (A \[Theta]dot^2 Sin[\[Theta]]+1/(1-A Cos[\[Theta]]^2) (\[Lambda]4 Cos[\[Theta]]-\[Lambda]2)+A Cos[\[Theta]] Sin[\[Theta]]),\[Theta]dot,1/(1-A Cos[\[Theta]]^2) (-(1/(1-A Cos[\[Theta]]^2))(-\[Lambda]2 Cos[\[Theta]]+\[Lambda]4 Cos[\[Theta]]^2)-Sin[\[Theta]]-A \[Theta]dot^2 Cos[\[Theta]] Sin[\[Theta]]),0,-\[Lambda]1,2/(A Cos[2 \[Theta]]+A-2)^3 (Cos[\[Theta]] (4 Sin[\[Theta]] (A \[Lambda]4^2 Cos[2 \[Theta]]+4 A \[Lambda]2^2+(A+2) \[Lambda]4^2)-(A Cos[2 \[Theta]]-3 A+2) (A Cos[2 \[Theta]]+A-2) (A \[Theta]dot^2 \[Lambda]2-\[Lambda]4))+A ((A-2) Cos[2 \[Theta]]+A) (A Cos[2 \[Theta]]+A-2) (\[Lambda]2-\[Theta]dot^2 \[Lambda]4)-4 \[Lambda]2 \[Lambda]4 Sin[\[Theta]] (3 A Cos[2 \[Theta]]+3 A+2)),4 /(A Cos[2 \[Theta]]+A-2) (A \[Theta]dot Sin[\[Theta]] (\[Lambda]2-\[Lambda]4 Cos[\[Theta]]))-\[Lambda]3};*)
 (*bcs={Subscript[x, 0]==ICs[[1]],Subscript[xdot, 0]==ICs[[2]],Subscript[x, n]==Subscript[xdot, n]==0,Subscript[\[Theta], 0]==ICs[[3]],Subscript[\[Theta]dot, 0]==ICs[[4]],Subscript[\[Theta]dot, n]==0,Subscript[\[Theta], n]==\[Pi]};eqns=Flatten[Join[bcs,Table[Thread[{Subscript[x, i],Subscript[xdot, i],Subscript[\[Theta], i],Subscript[\[Theta]dot, i],Subscript[\[Lambda]1, i],Subscript[\[Lambda]2, i],Subscript[\[Lambda]3, i],Subscript[\[Lambda]4, i]}==1/2 \[CapitalDelta]t (f[{Subscript[x, i-1],Subscript[xdot, i-1],Subscript[\[Theta], i-1],Subscript[\[Theta]dot, i-1],Subscript[\[Lambda]1, i-1],Subscript[\[Lambda]2, i-1],Subscript[\[Lambda]3, i-1],Subscript[\[Lambda]4, i-1]}]+f[{Subscript[x, i],Subscript[xdot, i],Subscript[\[Theta], i],Subscript[\[Theta]dot, i],Subscript[\[Lambda]1, i],Subscript[\[Lambda]2, i],Subscript[\[Lambda]3, i],Subscript[\[Lambda]4, i]}])+{Subscript[x, i-1],Subscript[xdot, i-1],Subscript[\[Theta], i-1],Subscript[\[Theta]dot, i-1],Subscript[\[Lambda]1, i-1],Subscript[\[Lambda]2, i-1],Subscript[\[Lambda]3, i-1],Subscript[\[Lambda]4, i-1]}],{i,1,n}]]];sv=Flatten[Table[{{Subscript[x, i],ICs[[1]]},{Subscript[xdot, i],ICs[[2]]},{Subscript[\[Theta], i],ICs[[3]]},{Subscript[\[Theta]dot, i],ICs[[4]]},{Subscript[\[Lambda]1, i],0},{Subscript[\[Lambda]2, i],0},{Subscript[\[Lambda]3, i],0},{Subscript[\[Lambda]4, i],0}},{i,0,n}],1];froot=FindRoot[eqns,sv];*)
 (*xff0=ListInterpolation[Table[Subscript[x, i],{i,0,n}]/. froot,{0,\[Tau]},InterpolationOrder->1];*)
@@ -75,7 +75,7 @@
 (**)
 (*{xff,xdotff,\[Theta]ff,\[Theta]dotff,uff}]*)
 (**)
-(*TestSwingUpGeneral[ICs_,\[Tau]_,\[Tau]1_,uff0_,A_]:=Module[{eq,init,x,xdot,\[Theta],\[Theta]dot,xs,xdots,\[Theta]s,\[Theta]dots,t,J},*)
+(*testSwingUp[ICs_,\[Tau]_,\[Tau]1_,uff0_,A_]:=Module[{eq,init,x,xdot,\[Theta],\[Theta]dot,xs,xdots,\[Theta]s,\[Theta]dots,t,J},*)
 (*eq={x'[t]==xdot[t],xdot'[t]==1/(1-A Cos[\[Theta][t]]^2) (uff0[t]+A \[Theta]dot[t]^2 Sin[\[Theta][t]]+A Cos[\[Theta][t]] Sin[\[Theta][t]]),\[Theta]'[t]==\[Theta]dot[t],\[Theta]dot'[t]==1/(1-A Cos[\[Theta][t]]^2) (-Sin[\[Theta][t]]-Cos[\[Theta][t]] (uff0[t]+A \[Theta]dot[t]^2 Sin[\[Theta][t]]))};*)
 (*init={x[0]==ICs[[1]],xdot[0]==ICs[[2]],\[Theta][0]==ICs[[3]],\[Theta]dot[0]==ICs[[4]]};*)
 (*{xs,xdots,\[Theta]s,\[Theta]dots}=NDSolveValue[{eq,init},{x,xdot,\[Theta],\[Theta]dot},{t,0,\[Tau]1},Method->{"DiscontinuityProcessing"->None}];*)
@@ -141,13 +141,13 @@
 (*us[t_]:=uff[t]+Piecewise[{{K3[t]*(\[Theta]ff[t]-\[Theta]s[t])+K4[t] *(\[Theta]dotff[t]-\[Theta]dots[t])+K1[t]*(xff[t]-xs[t])+K2[t] * (xdotff[t]-xdots[t]),0<=t<=\[Tau]}},\[Kappa]1(\[Theta]ff[t]-\[Theta]s[t])+\[Kappa]2 (\[Theta]dotff[t]-\[Theta]dots[t])+\[Kappa]3(xff[t]-xs[t])+\[Kappa]4 (xdotff[t]-xdots[t])];J = NIntegrate[us[t]^2,{t,0,\[Tau]}];*)
 (*{xs,xdots,\[Theta]s,\[Theta]dots,us,K1,K2,K3,K4,J}]*)
 (**)
-(*SimulateLinearFeedbackEnd[ICs_,n_,\[Tau]_,\[Tau]1_,A_]:= Module[{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a,*)
+(*simulateLinearFeedbackEnd[ICs_,n_,\[Tau]_,\[Tau]1_,A_]:= Module[{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a,*)
 (*	    x1b,xdot1b,\[Theta]1b,\[Theta]dot1b,u1b,*)
 (*	    x1c,xdot1c,\[Theta]1c,\[Theta]dot1c,u1c,*)
 (*	    p1a,p1b,p1c,J,J1},*)
 (**)
-(*{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a}=ffCartPendulumGeneral[ICs,n,\[Tau],\[Tau]1,A];  *)
-(*{x1b,xdot1b,\[Theta]1b,\[Theta]dot1b,u1b,J1}=TestSwingUpGeneral[ICs,\[Tau],\[Tau]1,u1a,A]; *)
+(*{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a}=ffCartPendulum[ICs,n,\[Tau],\[Tau]1,A];  *)
+(*{x1b,xdot1b,\[Theta]1b,\[Theta]dot1b,u1b,J1}=testSwingUp[ICs,\[Tau],\[Tau]1,u1a,A]; *)
 (*{x1c,xdot1c,\[Theta]1c,\[Theta]dot1c,u1c,J}=TestSwingUpGeneralFB[ICs,\[Tau],\[Tau]1,x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a,A];*)
 (**)
 (*p1a=Plot[{\[Theta]1a[t],u1a[t],x1a[t],\[Theta]dot1a[t],xdot1a[t]},{t,0,\[Tau]1},Filling->{2->Axis},PlotRange->{-4,4},PlotLegends->{"\[Theta]1a","u1a","x1a","\[Theta]dot1a","xdot1a"},PlotLabel->"Feedforward solution",AspectRatio->1/3,ImageSize->400,GridLines->{None,{-\[Pi],\[Pi]}}];*)
@@ -158,15 +158,15 @@
 (**)
 (*{J1,J,p1a,p1b,p1c}]*)
 (**)
-(*SimulateLQRFeedback[ICs_,n_,n2_,\[Tau]_,\[Tau]1_,A_]:= Module[{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a,*)
+(*simulateLQRFeedback[ICs_,n_,n2_,\[Tau]_,\[Tau]1_,A_]:= Module[{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a,*)
 (*	    x1b,xdot1b,\[Theta]1b,\[Theta]dot1b,u1b,*)
 (*	    x1c,xdot1c,\[Theta]1c,\[Theta]dot1c,u1c,*)
 (*	    p1a,p1b,p1c,KTable,K1,K2,K3,K4,J},*)
 (**)
-(*{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a}=ffCartPendulumGeneral[ICs,n,\[Tau],\[Tau]1,A];*)
+(*{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a}=ffCartPendulum[ICs,n,\[Tau],\[Tau]1,A];*)
 (*KTable = Table[CalculateGains[x1a[\[Tau]1/n2*i],xdot1a[\[Tau]1/n2*i],\[Theta]1a[\[Tau]1/n2*i],\[Theta]dot1a[\[Tau]1/n2*i],u1a[\[Tau]1/n2*i],A],{i,0,n2}]; *)
-(*{x1b,xdot1b,\[Theta]1b,\[Theta]dot1b,u1b,_}=TestSwingUpGeneral[ICs,\[Tau],\[Tau]1,u1a,A]; *)
-(*{x1c,xdot1c,\[Theta]1c,\[Theta]dot1c,u1c,K1,K2,K3,K4,J}=TestSwingUpGeneralFBNumeric[ICs,\[Tau],\[Tau]1,x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a,A,KTable,n2];;*)
+(*{x1b,xdot1b,\[Theta]1b,\[Theta]dot1b,u1b,_}=testSwingUp[ICs,\[Tau],\[Tau]1,u1a,A]; *)
+(*{x1c,xdot1c,\[Theta]1c,\[Theta]dot1c,u1c,K1,K2,K3,K4,J}=TestSwingUpGeneralFBNumeric[ICs,\[Tau],\[Tau]1,x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a,A,KTable,n2];*)
 (**)
 (*p1a=Plot[{\[Theta]1a[t],u1a[t],x1a[t],\[Theta]dot1a[t],xdot1a[t]},{t,0,\[Tau]1},Filling->{2->Axis},PlotRange->{-4,4},PlotLegends->{"\[Theta]1a","u1a","x1a","\[Theta]dot1a","xdot1a"},PlotLabel->"Feedforward solution",AspectRatio->1/3,ImageSize->400,GridLines->{None,{-\[Pi],\[Pi]}}];*)
 (**)
@@ -182,90 +182,85 @@
 (*EndPackage[]*)
 
 
-(* ::Section::Bold:: *)
+(* ::CodeText::Bold:: *)
 (*Testing Functions*)
 
 
-(*
-Use this to import the package. And use the following syntax to call the appropriate functions
-*)
-Get["D:\\Dhruv\\MITACS_Summer_22\\Codes\\cartPendulum.m"]
+(* ::CodeText::Bold:: *)
+(*n = 10; \[Tau] = 5; \[Tau]1 = \[Tau]*1.25 ; A = 0.2; *)
+(*ICs = {0.89486028245609, -0.9468360111172656, -0.002994757534002989, 1.677668990900959}; *)
+(*(*ICs = {0,0,0,0};*)*)
+(*{J1, J, p1a, p1b, p1c} = simulateLinearFeedbackEnd[ICs, n, \[Tau], \[Tau]1, A];*)
+(*Grid[{{p1a, p1b, p1c}}]*)
+(*J1*)
+(*J*)
 
 
-
-(* ::Code::Initialization::Bold:: *)
-n=10;\[Tau]=5;\[Tau]1= \[Tau]*1.25 ;A=0.2; 
-ICs = {0.89486028245609,-0.9468360111172656,-0.002994757534002989,1.677668990900959}; 
-(*ICs = {0,0,0,0};*)
-{J1,J,p1a,p1b,p1c} = SimulateLinearFeedbackEnd[ICs,n,\[Tau],\[Tau]1,A];
-Grid[{{p1a,p1b,p1c}}]
-J1
-J
+(* ::CodeText::Bold:: *)
+(*n = 5; \[Tau] = 5; \[Tau]1 = \[Tau]*1.25 ; n2 = 20;*)
+(*A = 0.2; ICs = {0.89486028245609, -0.9468360111172656, -0.002994757534002989, 1.677668990900959};*)
+(*{J, p1a, p1b, p1c} = simulateLQRFeedback[ICs, n, n2, \[Tau], \[Tau]1, A];*)
+(*Grid[{{p1a, p1b, p1c}}]*)
+(*J*)
 
 
-(* ::Code::Initialization::Bold:: *)
-n=7;\[Tau]=5;\[Tau]1= \[Tau]*1.25 ;n2 = 40;
-A=0.2; ICs = {0.89486028245609,-0.9468360111172656,-0.002994757534002989,1.677668990900959};
-{J,p1a,p1b,p1c} = SimulateLQRFeedback[ICs,n,n2,\[Tau],\[Tau]1,A];
-Grid[{{p1a,p1b,p1c}}]
-J
+(* ::CodeText:: *)
+(*(* Repeated Computations function*) (*No feedback *)*)
+(*n = 5; \[Tau] = 5; \[Tau]1 = \[Tau]*1.25 ; n2 = 20; M = 2;*)
+(*A = 0.2; initialConditions = {0, 0, 0, 0};*)
+(*MyAppend[f1_, f2_, T1_, dT_] := Module[{f},*)
+(*  f[t_] := Piecewise[{{f1[t], 0 <= t <= T1}, {f2[t - T1], T1 <= t <= T1 + dT}}];*)
+(*  f]*)
+(**)
+(*ICs = initialConditions;*)
+(*xs[t_] := 0;*)
+(*xdots[t_] := 0;*)
+(*\[Theta]s[t_] := 0;*)
+(*\[Theta]dots[t_] := 0;*)
+(*Js = 0;*)
+(*For[i = 0, i < M, i++,*)
+(* 	{x1a, xdot1a, \[Theta]1a, \[Theta]dot1a, u1a} = ffCartPendulum[ICs, n, \[Tau]*(1 - i/M), \[Tau]1, A];  *)
+(* 	Plot[{\[Theta]1a[t], u1a[t], x1a[t], \[Theta]dot1a[t], xdot1a[t]}, {t, 0, \[Tau]*(1 - i/M)}, PlotRange -> {-4, 4}, Filling -> {2 -> Axis}, PlotLegends -> {"\[Theta]1b", "u1b", "x1b", "\[Theta]dot1b", "xdot1b"}, PlotLabel -> "Intermediate Plots", AspectRatio -> 1/3, ImageSize -> 400, GridLines -> {None, {-\[Pi], \[Pi]}}]*)
+(*   *)
+(*   	If[i != M - 1,*)
+(*    	(*If condition is true*)*)
+(*    		{x1b, xdot1b, \[Theta]1b, \[Theta]dot1b, u1b, J} = testSwingUp[ICs, \[Tau]*1/M, \[Tau]*1/M, u1a, A]; *)
+(*    		ICs = {x1b[\[Tau]*1/M], xdot1b[\[Tau]*1/M], \[Theta]1b[\[Tau]*1/M], \[Theta]dot1b[\[Tau]*1/M]};*)
+(*    		xs = MyAppend[xs, x1b, \[Tau]*i/M, \[Tau]*1/M];*)
+(*    		xdots = MyAppend[xdots, xdot1b, \[Tau]*i/M, \[Tau]*1/M];*)
+(*    		\[Theta]s = MyAppend[\[Theta]s, \[Theta]1b, \[Tau]*i/M, \[Tau]*1/M];*)
+(*    		\[Theta]dots = MyAppend[\[Theta]dots, \[Theta]dot1b, \[Tau]*i/M, \[Tau]*1/M];*)
+(*    		us = MyAppend[us, u1b, \[Tau]*i/M, \[Tau]*1/M];*)
+(*    		Js = Js + J;*)
+(*    	,*)
+(*    	(*If condition is false*)*)
+(*    		{x1b, xdot1b, \[Theta]1b, \[Theta]dot1b, u1b, J} = testSwingUp[ICs, \[Tau]*1/M, \[Tau]*1/M + \[Tau]1 - \[Tau], u1a, A]; *)
+(*    		xs = MyAppend[xs, x1b, \[Tau]*i/M, \[Tau]*1/M + \[Tau]1 - \[Tau]];*)
+(*    		xdots = MyAppend[xdots, xdot1b, \[Tau]*i/M, \[Tau]*1/M + \[Tau]1 - \[Tau]];*)
+(*    		\[Theta]s = MyAppend[\[Theta]s, \[Theta]1b, \[Tau]*i/M, \[Tau]*1/M + \[Tau]1 - \[Tau]];*)
+(*    		\[Theta]dots = MyAppend[\[Theta]dots, \[Theta]dot1b, \[Tau]*i/M, \[Tau]*1/M + \[Tau]1 - \[Tau]];*)
+(*    		us = MyAppend[us, u1b, \[Tau]*i/M, \[Tau]*1/M + \[Tau]1 - \[Tau]];*)
+(*    	]	*)
+(*  // Print]*)
+(*p1b = Plot[{\[Theta]s[t], us[t], xs[t], \[Theta]dots[t], xdots[t]}, {t, 0, \[Tau]1}, PlotRange -> {-4, 4}, Filling -> {2 -> Axis}, PlotLegends -> {"\[Theta]1b", "u1b", "x1b", "\[Theta]dot1b", "xdot1b"}, PlotLabel -> "Test on dynamics", AspectRatio -> 1/3, ImageSize -> 400, GridLines -> {None, {-\[Pi], \[Pi]}}]*)
+(*Js*)
 
 
-(* Repeated Computations function*) (*No feedback *)
-n=5;\[Tau]=5;\[Tau]1= \[Tau]*1.25 ;n2 = 20; M = 2;
-A=0.2; initialConditions = {0,0,0,0};
-MyAppend[f1_,f2_,T1_,dT_]:= Module[{f},
-f[t_] := Piecewise[{{f1[t],0<= t <= T1},{f2[t-T1],T1<= t<= T1 + dT}}];
-f]
-
-ICs = initialConditions;
-xs[t_] := 0;
-xdots[t_] := 0;
-\[Theta]s[t_] := 0;
-\[Theta]dots[t_] := 0;
-Js = 0;
-For[i = 0, i < M, i++,
-	{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a}=ffCartPendulumGeneral[ICs,n,\[Tau]*(1-i/M),\[Tau]1,A];  
-	Plot[{\[Theta]1a[t],u1a[t],x1a[t],\[Theta]dot1a[t],xdot1a[t]},{t,0,\[Tau]*(1-i/M)},PlotRange->{-4,4},Filling->{2->Axis},PlotLegends->{"\[Theta]1b","u1b","x1b","\[Theta]dot1b","xdot1b"},PlotLabel->"Intermediate Plots",AspectRatio->1/3,ImageSize->400,GridLines->{None,{-\[Pi],\[Pi]}}]
-
-	If[i != M-1,
-	(*If condition is true*)
-		{x1b,xdot1b,\[Theta]1b,\[Theta]dot1b,u1b,J}=TestSwingUpGeneral[ICs,\[Tau]*1/M,\[Tau]*1/M,u1a,A]; 
-		ICs = {x1b[\[Tau]*1/M],xdot1b[\[Tau]*1/M],\[Theta]1b[\[Tau]*1/M],\[Theta]dot1b[\[Tau]*1/M]};
-		xs = MyAppend[xs,x1b,\[Tau]*i/M,\[Tau]*1/M];
-		xdots = MyAppend[xdots,xdot1b,\[Tau]*i/M,\[Tau]*1/M];
-		\[Theta]s = MyAppend[\[Theta]s,\[Theta]1b,\[Tau]*i/M,\[Tau]*1/M];
-		\[Theta]dots = MyAppend[\[Theta]dots,\[Theta]dot1b,\[Tau]*i/M,\[Tau]*1/M];
-		us = MyAppend[us,u1b,\[Tau]*i/M,\[Tau]*1/M];
-		Js = Js + J;
-	,
-	(*If condition is false*)
-		{x1b,xdot1b,\[Theta]1b,\[Theta]dot1b,u1b,J}=TestSwingUpGeneral[ICs,\[Tau]*1/M,\[Tau]*1/M + \[Tau]1 - \[Tau],u1a,A]; 
-		xs = MyAppend[xs,x1b,\[Tau]*i/M,\[Tau]*1/M + \[Tau]1 - \[Tau]];
-		xdots = MyAppend[xdots,xdot1b,\[Tau]*i/M,\[Tau]*1/M + \[Tau]1 - \[Tau]];
-		\[Theta]s = MyAppend[\[Theta]s,\[Theta]1b,\[Tau]*i/M,\[Tau]*1/M + \[Tau]1 - \[Tau]];
-		\[Theta]dots = MyAppend[\[Theta]dots,\[Theta]dot1b,\[Tau]*i/M,\[Tau]*1/M + \[Tau]1 - \[Tau]];
-		us = MyAppend[us,u1b,\[Tau]*i/M,\[Tau]*1/M + \[Tau]1 - \[Tau]];
-	]	
-//Print]
-p1b=Plot[{\[Theta]s[t],us[t],xs[t],\[Theta]dots[t],xdots[t]},{t,0,\[Tau]1},PlotRange->{-4,4},Filling->{2->Axis},PlotLegends->{"\[Theta]1b","u1b","x1b","\[Theta]dot1b","xdot1b"},PlotLabel->"Test on dynamics",AspectRatio->1/3,ImageSize->400,GridLines->{None,{-\[Pi],\[Pi]}}]
-Js
+(* ::CodeText:: *)
+(*ICs*)
+(*{x1a, xdot1a, \[Theta]1a, \[Theta]dot1a, u1a} = ffCartPendulum[ICs, n, \[Tau], \[Tau]1, A];  *)
+(*Plot[{\[Theta]1a[t], u1a[t], x1a[t], \[Theta]dot1a[t], xdot1a[t]}, {t, 0, \[Tau]}, PlotRange -> {-4, 4}, Filling -> {2 -> Axis}, PlotLegends -> {"\[Theta]1b", "u1b", "x1b", "\[Theta]dot1b", "xdot1b"}, PlotLabel -> "Intermediate Plots", AspectRatio -> 1/3, ImageSize -> 400, GridLines -> {None, {-\[Pi], \[Pi]}}]*)
+(**)
 
 
-ICs
-{x1a,xdot1a,\[Theta]1a,\[Theta]dot1a,u1a}=ffCartPendulumGeneral[ICs,n,\[Tau],\[Tau]1,A];  
-Plot[{\[Theta]1a[t],u1a[t],x1a[t],\[Theta]dot1a[t],xdot1a[t]},{t,0,\[Tau]},PlotRange->{-4,4},Filling->{2->Axis},PlotLegends->{"\[Theta]1b","u1b","x1b","\[Theta]dot1b","xdot1b"},PlotLabel->"Intermediate Plots",AspectRatio->1/3,ImageSize->400,GridLines->{None,{-\[Pi],\[Pi]}}]
-
-
-
-(* ::Code::Initialization::Bold:: *)
-n=40;\[Tau]=5;\[Tau]1= \[Tau]*1.25 ;n2 = 20; M = 2;
-A=0.2;
-ICs = {-0.04586643004043421,-1.5525579578695232,0.7616256926010762,1.9326806921803874};
-{J1,J,p1a,p1b,p1c} = SimulateLinearFeedbackEnd[ICs,n,\[Tau],\[Tau]1,A];
-Grid[{{p1a,p1b,p1c}}]
-J1
-J
-
-
-
+(* ::CodeText::Bold:: *)
+(*n = 40; \[Tau] = 5; \[Tau]1 = \[Tau]*1.25 ; n2 = 20; M = 2;*)
+(*A = 0.2;*)
+(*ICs = {-0.04586643004043421, -1.5525579578695232, 0.7616256926010762, 1.9326806921803874};*)
+(*{J1, J, p1a, p1b, p1c} = simulateLinearFeedbackEnd[ICs, n, \[Tau], \[Tau]1, A];*)
+(*Grid[{{p1a, p1b, p1c}}]*)
+(*J1*)
+(*J*)
+(**)
+(**)
+(**)
